406611966a9aa808cf14198779af63e2b802e6b9
mspmjl <867181563@qq.com>
2021-01-30 16:26:02 +0800

    add

--- src/main/java/com/interview/multithread/VolatileDemo.java
+++ src/main/java/com/interview/multithread/VolatileDemo.java
@@ -14,6 +14,7 @@ class MyData {
 public class VolatileDemo {
 
     public static void main(String[] args) {
+
         MyData myData = new MyData();
         new Thread(() -> {
             myData.add();
